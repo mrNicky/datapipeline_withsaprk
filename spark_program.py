7d067137a63c781e4b311516e8434850ab9782e6
zakariat <tahizak@gmail.com>
2020-02-03 16:06:00 +0000

    second

--- spark_program.py
+++ spark_program.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
 	.appName("reading csv") \
 	.getOrCreate()
 
-data_file = 'supermarket_sales.csv'
-sData = scSpark.read.csv(data_file, header=True, sep=",").cache()
-extract = sData.where(sData["Total"] > 100)
-extract.show()
-print("Total Records of our data : {}".format(extract.count()))
+	data_file = 'supermarket_sales.csv'
+	sData = scSpark.read.csv(data_file, header=True, sep=",").cache()
+	extract = sData.where(sData["Total"] > 100)
+	extract.show()
+	print("Total Records of our data : {}".format(extract.count()))
 
-print("TRANSFORM")
-extract.registerTempTable("sales")
-output = scSpark.sql("SELECT * FROM sales")
-output.show()
+	print("TRANSFORM")
+	extract.registerTempTable("sales")
+	output = scSpark.sql("SELECT * FROM sales")
+	output.show()
 
-print("LOAD") 
-output.write.format("json").save("filtered.json") 
-print("SAVING IN JSON in file")
+	print("LOAD") 
+	output.write.format("json").save("filtered.json") 
+	print("SAVING IN JSON in file")
